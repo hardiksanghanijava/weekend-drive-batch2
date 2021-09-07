936ed9b1b9c56a52090f6501288501be83bcea99
tarunpatel7103 <90174846+tarunpatel7103@users.noreply.github.com>
2021-09-07 14:59:30 +0530

    basic structure

---
+++ RoundApi/src/test/java/com/miniproject/restApi/RoundApi/RoundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.restApi.RoundApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
